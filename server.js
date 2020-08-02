83dbb886c6bd6c2f131dbb1e8c9076e2be2faad4
Jomark Pangan <jomarkrazonpangan@gmail.com>
2020-08-03 00:02:15 +0800

    Deploying to heroku

--- server.js
+++ server.js
@@ -11,8 +11,6 @@ app.use(cors());
 
 const PORT = process.env.PORT || 5000;
 
-
-
 // set up mongoose
 
 mongoose.connect(
@@ -32,5 +30,11 @@ mongoose.connect(
 
 app.use("/users", require("./routes/userRouter"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build/"));
 
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
 app.listen(PORT, () => console.log(`The server has started on port: ${PORT}`));
